5d37fb0f0fcaf3f9bc3735db601d2bec4e924f66
afjke36900 <55038688+afjke36900@users.noreply.github.com>
2020-04-28 16:20:02 +0800

    課後練習_2
    開啟專案

---
+++ ClassField2/Assets/Bat.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Bat : MonoBehaviour
+{
+    [Header("血量")]
+    public int hp = 300;
+    
+    [Header("攻擊力")]
+    public int attack = 50;
+
+    [Header("治癒力")]
+    public int cure = 30;
+
+}
